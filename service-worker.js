0afe31aab53d87629a8ee1e6fd526f310fe1ee63
Jeong Seung Min <98789188+sminiduck@users.noreply.github.com>
2024-10-06 06:28:13 +0000

    manifest 오타 수정

--- service-worker.js
+++ service-worker.js
@@ -4,12 +4,32 @@ self.addEventListener('install', function(event) {
         return cache.addAll([
           '/',
           '/index.html',
-          '/styles.css',
-          '/script.js',
-          'icon-192x192.png',
-          'icon-512x512.png'
+          '/main.css',
+          'cone.png'
         ]);
       })
+    );`
+    self.addEventListener('install', function(event) {
+        event.waitUntil(
+          caches.open(CACHE_NAME)
+            .then(function(cache) {
+              console.log('Opened cache');
+              return cache.addAll(urlsToCache);
+            })
+        );
+      });
+      
+    self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            if (response) {
+            return response;
+            }
+            return fetch(event.request);
+        }
+        )
     );
+    });
 });
   
