5ce88cf2683db2a4481cf54e270e2f4fe2bd61e2
Jeong Seung Min <98789188+sminiduck@users.noreply.github.com>
2024-10-06 06:35:44 +0000

    pwa수정02

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,27 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/main.css',
+  '/cone.png'
+];
+
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('my-cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/main.css',
-          'cone.png'
-        ]);
-      })
-    );`
-    self.addEventListener('install', function(event) {
-        event.waitUntil(
-          caches.open(CACHE_NAME)
-            .then(function(cache) {
-              console.log('Opened cache');
-              return cache.addAll(urlsToCache);
-            })
-        );
-      });
-      
-    self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-        .then(function(response) {
-            if (response) {
-            return response;
-            }
-            return fetch(event.request);
-        }
-        )
-    );
-    });
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
-  
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
